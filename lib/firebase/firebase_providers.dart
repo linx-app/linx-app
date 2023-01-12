87ce1a9cf18eccbe03a68603afca98e32dafb117
timmytruong <timmy7truong@gmail.com>
2023-01-12 15:52:53 -0500

    Add firebase storage provider

--- lib/firebase/firebase_providers.dart
+++ lib/firebase/firebase_providers.dart
@@ -1,16 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final firebaseAuthProvider = Provider<FirebaseAuth>(
-        (ref) => FirebaseAuth.instance
-);
+final firebaseAuthProvider =
+    Provider<FirebaseAuth>((ref) => FirebaseAuth.instance);
 
 final authStateChangesProvider = StreamProvider<User?>(
-        (ref) => ref.watch(firebaseAuthProvider).authStateChanges()
-);
+    (ref) => ref.watch(firebaseAuthProvider).authStateChanges());
 
-final firestoreProvider = Provider<FirebaseFirestore>(
-        (ref) => FirebaseFirestore.instance
-);
+final firestoreProvider =
+    Provider<FirebaseFirestore>((ref) => FirebaseFirestore.instance);
 
+final firebaseStorageProvider =
+    Provider<FirebaseStorage>((ref) => FirebaseStorage.instance);
