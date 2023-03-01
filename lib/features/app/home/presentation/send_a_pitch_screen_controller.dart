1d94fde9784d7d481e15ca844cdcb1cfed5b072e
timmytruong <timmy7truong@gmail.com>
2023-02-28 21:12:49 -0500

    Add base for fcm and cloud functions

--- lib/features/app/home/presentation/send_a_pitch_screen_controller.dart
+++ lib/features/app/home/presentation/send_a_pitch_screen_controller.dart
@@ -2,27 +2,29 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:linx/features/app/home/domain/model/request.dart';
 import 'package:linx/features/app/home/domain/send_request_service.dart';
 import 'package:linx/features/user/domain/model/linx_user.dart';
+import 'package:linx/features/user/domain/user_service.dart';
 
 final sendAPitchScreenControllerProvider =
-    StateNotifierProvider<SendAPitchScreenController, SendAPitchUiState>(
+    StateNotifierProvider.autoDispose<SendAPitchScreenController, SendAPitchUiState>(
   (ref) => SendAPitchScreenController(
     ref.read(SendRequestService.provider),
+    ref.read(UserService.provider)
   ),
 );
 
 class SendAPitchScreenController extends StateNotifier<SendAPitchUiState> {
   final SendRequestService _sendRequestService;
+  final UserService _userService;
 
-  SendAPitchScreenController(this._sendRequestService)
+  SendAPitchScreenController(this._sendRequestService, this._userService)
       : super(SendAPitchUiState.idle);
 
   Future<void> sendPitch({
     required LinxUser receiver,
-    required LinxUser sender,
     required String message,
   }) async {
     state = SendAPitchUiState.loading;
-
+    var sender = await _userService.fetchUserProfile();
     var request = Request(
       sender: sender,
       receiver: receiver,
